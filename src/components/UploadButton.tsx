e1c2c5517f95e487efef329bdfed08a5d2bff7e3
Shreeraj Shinde <shreerajs123@gmail.com>
2025-04-09 12:15:04 +0530

    Added Files model and files fetching functionality

---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+const UploadButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(visible) => {
+        if (!visible) setIsOpen(visible);
+      }}
+    >
+      <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+        <Button>Upload PDF</Button>
+      </DialogTrigger>
+    </Dialog>
+  );
+};
+
+export default UploadButton;
